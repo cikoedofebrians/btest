c94f42f2c83cf9da86dc2be26ad1faddcebd1d01
cikoedofebrian <ciko.edo.febrians@gmail.com>
2023-11-01 17:00:36 +0700

    Initial Commit

---
+++ btest/btestApp.swift
@@ -0,0 +1,17 @@
+//
+//  btestApp.swift
+//  btest
+//
+//  Created by Ciko Edo Febrian on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct btestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
